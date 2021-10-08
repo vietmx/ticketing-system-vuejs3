2adccd07a409b422e64f69e55d583018812b1fce
VietMX <vietmx@fincorp.com>
2021-10-08 12:11:35 +0700

    init source code

---
+++ src/components/Events.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row pb-5">
+    <Event v-for="(event, i) in events" :key="i" :event="event" />
+  </div>
+</template>
+
+<script>
+import Event from "./Event.vue";
+import { computed } from "vue";
+import { mapState, useStore } from "vuex";
+export default {
+  name: "Events",
+  components: { Event },
+  setup() {
+    const store = useStore();
+    return {
+      events: computed(() => store.state.events),
+    };
+  },
+};
+</script>
